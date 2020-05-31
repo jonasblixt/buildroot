832d21db822c513add73e0509fc96b749c3d0ac9
Jonas Blixt <jonpe960@gmail.com>
2020-05-31 14:24:10 +0200

    Add package punchboot-tools

---
+++ package/punchboot-tools/punchboot-tools.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# Punchboot tools
+#
+################################################################################
+
+PUNCHBOOT_TOOLS_VERSION = v0.1.14
+PUNCHBOOT_TOOLS_SITE = https://github.com/jonasblixt/punchboot-tools.git
+PUNCHBOOT_TOOLS_SITE_METHOD = git
+PUNCHBOOT_TOOLS_INSTALL_STAGING = YES
+PUNCHBOOT_TOOLS_LICENSE = BSD-3-Clause
+PUNCHBOOT_TOOLS_LICENSE_FILES = License.txt
+PUNCHBOOT_TOOLS_AUTORECONF = YES
+PUNCHBOOT_TOOLS_DEPENDENCIES = libusb
+
+$(eval $(host-autotools-package))
